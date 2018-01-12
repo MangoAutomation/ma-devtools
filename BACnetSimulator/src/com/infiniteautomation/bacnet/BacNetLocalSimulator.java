e85bb0cc46ed91e122db3af4b71939e124c70a6b
terrypacker <terrypacker@users.noreply.github.com>
2018-01-12 13:21:12 -1000

    Mavenize dependencies and add Schedule,Calendar objects.

--- BACnetSimulator/src/com/infiniteautomation/bacnet/BacNetLocalSimulator.java
+++ BACnetSimulator/src/com/infiniteautomation/bacnet/BacNetLocalSimulator.java
@@ -1,5 +1,6 @@
 /**
  * Copyright (C) 2013 Infinite Automation Software. All rights reserved.
+ * 
  * @author Terry Packer
  */
 package com.infiniteautomation.bacnet;
@@ -12,34 +13,43 @@ import java.util.List;
 
 import com.serotonin.bacnet4j.LocalDevice;
 import com.serotonin.bacnet4j.RemoteDevice;
+import com.serotonin.bacnet4j.enums.DayOfWeek;
+import com.serotonin.bacnet4j.enums.Month;
 import com.serotonin.bacnet4j.event.DeviceEventAdapter;
 import com.serotonin.bacnet4j.exception.BACnetException;
+import com.serotonin.bacnet4j.exception.BACnetServiceException;
 import com.serotonin.bacnet4j.npdu.Network;
-import com.serotonin.bacnet4j.npdu.ip.IpNetwork;
+import com.serotonin.bacnet4j.npdu.ip.IpNetworkBuilder;
 import com.serotonin.bacnet4j.npdu.mstp.MstpNetwork;
 import com.serotonin.bacnet4j.npdu.mstp.MstpNode;
 import com.serotonin.bacnet4j.npdu.mstp.SlaveNode;
+import com.serotonin.bacnet4j.obj.AnalogValueObject;
 import com.serotonin.bacnet4j.obj.BACnetObject;
-import com.serotonin.bacnet4j.service.acknowledgement.ReadPropertyAck;
+import com.serotonin.bacnet4j.obj.CalendarObject;
+import com.serotonin.bacnet4j.obj.ScheduleObject;
 import com.serotonin.bacnet4j.service.confirmed.ReadPropertyRequest;
 import com.serotonin.bacnet4j.service.unconfirmed.WhoIsRequest;
+import com.serotonin.bacnet4j.transport.DefaultTransport;
 import com.serotonin.bacnet4j.transport.Transport;
 import com.serotonin.bacnet4j.type.constructed.Address;
-import com.serotonin.bacnet4j.type.constructed.LogRecord;
-import com.serotonin.bacnet4j.type.constructed.Scale;
+import com.serotonin.bacnet4j.type.constructed.BACnetArray;
+import com.serotonin.bacnet4j.type.constructed.CalendarEntry;
+import com.serotonin.bacnet4j.type.constructed.DailySchedule;
+import com.serotonin.bacnet4j.type.constructed.DateRange;
+import com.serotonin.bacnet4j.type.constructed.DeviceObjectPropertyReference;
 import com.serotonin.bacnet4j.type.constructed.SequenceOf;
-import com.serotonin.bacnet4j.type.constructed.ServicesSupported;
-import com.serotonin.bacnet4j.type.constructed.StatusFlags;
+import com.serotonin.bacnet4j.type.constructed.SpecialEvent;
+import com.serotonin.bacnet4j.type.constructed.TimeValue;
+import com.serotonin.bacnet4j.type.constructed.WeekNDay;
+import com.serotonin.bacnet4j.type.constructed.WeekNDay.WeekOfMonth;
 import com.serotonin.bacnet4j.type.enumerated.EngineeringUnits;
-import com.serotonin.bacnet4j.type.enumerated.EventState;
-import com.serotonin.bacnet4j.type.enumerated.LoggingType;
 import com.serotonin.bacnet4j.type.enumerated.ObjectType;
 import com.serotonin.bacnet4j.type.enumerated.PropertyIdentifier;
-import com.serotonin.bacnet4j.type.enumerated.Segmentation;
-import com.serotonin.bacnet4j.type.primitive.CharacterString;
+import com.serotonin.bacnet4j.type.primitive.Date;
 import com.serotonin.bacnet4j.type.primitive.ObjectIdentifier;
 import com.serotonin.bacnet4j.type.primitive.OctetString;
 import com.serotonin.bacnet4j.type.primitive.Real;
+import com.serotonin.bacnet4j.type.primitive.Time;
 import com.serotonin.bacnet4j.type.primitive.Unsigned16;
 import com.serotonin.bacnet4j.type.primitive.UnsignedInteger;
 import com.serotonin.bacnet4j.util.RequestUtils;
@@ -52,80 +62,128 @@ public class BacNetLocalSimulator {
     static LocalDevice localDevice;
 
     public static void main(String[] args) throws Exception {
-    	boolean mstp = false;
-    	
-        Network network;
-        
-        if(mstp){
-	        //Create an MSTP local device 
-        	System.out.println("Starting MSTP Network");
-	        byte[] bytes = new byte[100];
-	        InputStream is = new ByteArrayInputStream(bytes);
-	        OutputStream os = new ByteArrayOutputStream();
-	        
-	        MstpNode node = new SlaveNode(is,os,(byte)3);
-	        network = new MstpNetwork(node, 20);
-        }else{
-        	System.out.println("Starting IP Network");
-        	network = new IpNetwork("255.255.255.255", 47808,"0.0.0.0",20);
-        }
-        
-        
-        
-        
+        boolean mstp = false;
 
-        Transport transport = new Transport(network);
-        //        transport.setTimeout(15000);
-        //        transport.setSegTimeout(15000);
+        Network network;
+
+        if (mstp) {
+            // Create an MSTP local device
+            System.out.println("Starting MSTP Network");
+            byte[] bytes = new byte[100];
+            InputStream is = new ByteArrayInputStream(bytes);
+            OutputStream os = new ByteArrayOutputStream();
+
+            MstpNode node = new SlaveNode("SlaveNode", is, os, (byte) 3);
+            network = new MstpNetwork(node, 20);
+        } else {
+            System.out.println("Starting IP Network");
+            network = new IpNetworkBuilder()
+                    .withPort(47808)
+                    .withLocalBindAddress("0.0.0.0")
+                    .withLocalNetworkNumber(20)
+                    .withBroadcast("255.255.255.255", 255)
+                    .build();
+                    
+       }
+
+
+
+        Transport transport = new DefaultTransport(network);
+        // transport.setTimeout(15000);
+        // transport.setSegTimeout(15000);
         localDevice = new LocalDevice(1234, transport);
         try {
             localDevice.initialize();
             localDevice.getEventHandler().addListener(new Listener());
             localDevice.sendGlobalBroadcast(new WhoIsRequest());
-            
-            //Create an analog Input object type
+
+            // Create an analog Input object type
             ObjectIdentifier id = new ObjectIdentifier(ObjectType.analogInput, 0);
             BACnetObject obj = new BACnetObject(localDevice, id);
-            //obj.setProperty(PropertyIdentifier.description, new CharacterString("Analoge Description"));
-            
-            obj.setProperty(PropertyIdentifier.objectName, new CharacterString("Analoge Obj Name"));
-            obj.setProperty(PropertyIdentifier.presentValue, new Real(1.0f));
-            //obj.setProperty(PropertyIdentifier.modelName, new CharacterString("model name"));
-            obj.setProperty(PropertyIdentifier.units, new EngineeringUnits(1));
-            //obj.setProperty(PropertyIdentifier.inactiveText, new CharacterString("inactive text"));
-            localDevice.addObject(obj);           
 
-            //Create Accumulator Object Type
+            localDevice.addObject(obj);
+
+            // Create Accumulator Object Type
             ObjectIdentifier accumulatorId = new ObjectIdentifier(ObjectType.accumulator, 1);
             BACnetObject accumulator = new BACnetObject(localDevice, accumulatorId);
-            accumulator.setProperty(PropertyIdentifier.objectName, new CharacterString("Accumulator Obj Name"));
-            accumulator.setProperty(PropertyIdentifier.scale, new Scale(new Real(1.0f)));
-            accumulator.setProperty(PropertyIdentifier.maxPresValue, new UnsignedInteger(100));
-            accumulator.setProperty(PropertyIdentifier.presentValue, new UnsignedInteger(1));
             localDevice.addObject(accumulator);
-            
-            
-            //Create a trend Log object type
+
+
+            // Create a trend Log object type
             ObjectIdentifier trendLogId = new ObjectIdentifier(ObjectType.trendLog, 1);
             BACnetObject trendLog = new BACnetObject(localDevice, trendLogId);
-            trendLog.setProperty(PropertyIdentifier.objectName, new CharacterString("Trend Log Name"));
-            trendLog.setProperty(PropertyIdentifier.enable, new com.serotonin.bacnet4j.type.primitive.Boolean(true));
-            trendLog.setProperty(PropertyIdentifier.stopWhenFull, new com.serotonin.bacnet4j.type.primitive.Boolean(true));
-            trendLog.setProperty(PropertyIdentifier.bufferSize, new UnsignedInteger(100));
-            trendLog.setProperty(PropertyIdentifier.logBuffer, new SequenceOf<LogRecord>());
-            trendLog.setProperty(PropertyIdentifier.recordCount, new UnsignedInteger(100));
-            trendLog.setProperty(PropertyIdentifier.totalRecordCount, new UnsignedInteger(100));
-            trendLog.setProperty(PropertyIdentifier.eventState, EventState.normal);
-            trendLog.setProperty(PropertyIdentifier.loggingType, LoggingType.polled);
-            trendLog.setProperty(PropertyIdentifier.statusFlags, new StatusFlags(false, false, false, true));
             localDevice.addObject(trendLog);
 
-            
+            // Create a Calendar
+            CalendarObject co = createCalendar();
+            createSchedule(co);
+
             Thread.sleep(200000000);
-        }
-        finally {
+        } finally {
             localDevice.terminate();
         }
+    }
+
+    public static ScheduleObject createSchedule(CalendarObject co) throws BACnetServiceException {
+        final DateRange effectivePeriod = new DateRange(Date.UNSPECIFIED, Date.UNSPECIFIED);
+        final BACnetArray<DailySchedule> weeklySchedule = new BACnetArray<>( //
+                new DailySchedule(new SequenceOf<>(new TimeValue(new Time(8, 0, 0, 0), new Real(10)),
+                        new TimeValue(new Time(17, 0, 0, 0), new Real(11)))), //
+                new DailySchedule(new SequenceOf<>(new TimeValue(new Time(8, 0, 0, 0), new Real(12)),
+                        new TimeValue(new Time(17, 0, 0, 0), new Real(13)))), //
+                new DailySchedule(new SequenceOf<>(new TimeValue(new Time(8, 0, 0, 0), new Real(14)),
+                        new TimeValue(new Time(17, 0, 0, 0), new Real(15)))), //
+                new DailySchedule(new SequenceOf<>(new TimeValue(new Time(9, 0, 0, 0), new Real(16)),
+                        new TimeValue(new Time(20, 0, 0, 0), new Real(17)))), //
+                new DailySchedule(new SequenceOf<>(new TimeValue(new Time(9, 0, 0, 0), new Real(18)),
+                        new TimeValue(new Time(21, 30, 0, 0), new Real(19)))), //
+                new DailySchedule(new SequenceOf<TimeValue>()), //
+                new DailySchedule(new SequenceOf<TimeValue>()));
+        final SequenceOf<SpecialEvent> exceptionSchedule = new SequenceOf<>( //
+                new SpecialEvent(co.getId(),
+                        new SequenceOf<>(new TimeValue(new Time(8, 0, 0, 0), new Real(20)),
+                                new TimeValue(new Time(22, 0, 0, 0), new Real(21))),
+                        new UnsignedInteger(10)), // Calendar
+                new SpecialEvent(co.getId(),
+                        new SequenceOf<>(new TimeValue(new Time(13, 0, 0, 0), new Real(22)),
+                                new TimeValue(new Time(14, 0, 0, 0), new Real(23))),
+                        new UnsignedInteger(7)), // Calendar
+                new SpecialEvent(new CalendarEntry(new Date(-1, null, 8, DayOfWeek.WEDNESDAY)),
+                        new SequenceOf<>(new TimeValue(new Time(10, 30, 0, 0), new Real(24)),
+                                new TimeValue(new Time(17, 0, 0, 0), new Real(25))),
+                        new UnsignedInteger(6)) // 7th is a Wednesday
+        );
+        
+        final AnalogValueObject av0 = new AnalogValueObject(localDevice, 2, "av2", 98, EngineeringUnits.amperes, false)
+                .supportCommandable(-2);
+        final AnalogValueObject av1 = new AnalogValueObject(localDevice, 3, "av3", 99, EngineeringUnits.amperesPerMeter, false)
+                .supportCommandable(-1);
+
+        final SequenceOf<DeviceObjectPropertyReference> listOfObjectPropertyReferences = new SequenceOf<>( //
+                new DeviceObjectPropertyReference(av0.getId(), PropertyIdentifier.presentValue, null, null), //
+                new DeviceObjectPropertyReference(av1.getId(), PropertyIdentifier.presentValue, null, null) //
+        );
+
+        final ScheduleObject so = new ScheduleObject(localDevice, 0, "sch0", effectivePeriod, weeklySchedule, exceptionSchedule,
+                new Real(8), listOfObjectPropertyReferences, 12, false);
+        return so;
+    }
+    /**
+     * Create an example schedule
+     * @return
+     * @throws BACnetServiceException
+     */
+    public static CalendarObject createCalendar() throws BACnetServiceException {
+        final CalendarEntry ce = new CalendarEntry(
+                new WeekNDay(Month.UNSPECIFIED, WeekOfMonth.days22to28, DayOfWeek.WEDNESDAY)); // The Wednesday during the 4th week of each month.
+        final SequenceOf<CalendarEntry> dateList = new SequenceOf<>( //
+                new CalendarEntry(new Date(-1, null, -1, DayOfWeek.FRIDAY)), // Every Friday.
+                new CalendarEntry(
+                        new DateRange(new Date(-1, Month.NOVEMBER, -1, null), new Date(-1, Month.FEBRUARY, -1, null))), // November to February
+                ce);
+        final CalendarObject co = new CalendarObject(localDevice, 0, "cal0", dateList);
+
+        return co;
     }
 
     static class Listener extends DeviceEventAdapter {
@@ -134,87 +192,88 @@ public class BacNetLocalSimulator {
             try {
                 System.out.println("IAm received from " + d);
                 System.out.println("Segmentation: " + d.getSegmentationSupported());
-                d.setSegmentationSupported(Segmentation.noSegmentation);
 
-                Address a = new Address(new Unsigned16(0), new OctetString(new byte[] { (byte) 0xc0, (byte) 0xa8, 0x1,
-                        0x5, (byte) 0xba, (byte) 0xc0 }));
+                Address a = new Address(new Unsigned16(0), new OctetString(
+                        new byte[] {(byte) 0xc0, (byte) 0xa8, 0x1, 0x5, (byte) 0xba, (byte) 0xc0}));
 
                 System.out.println("Equals: " + a.equals(d.getAddress()));
                 ExtendedDeviceInformationExecutor e1 = new ExtendedDeviceInformationExecutor(d);
                 e1.start();
-                
+
                 System.out.println("Done getting extended information");
 
                 IncomingMessageExecutor e = new IncomingMessageExecutor(d);
                 e.start();
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
-    static class ExtendedDeviceInformationExecutor extends Thread{
-    	
-    	RemoteDevice d;
-    	
-    	public ExtendedDeviceInformationExecutor(RemoteDevice d){
-    		this.d = d;
-    	}
-    	
-    	@Override
-    	public void run() {
-    		try{
-		        ObjectIdentifier oid = d.getObjectIdentifier();
-		
-		        // Get the device's supported services
-		        System.out.println("protocolServicesSupported");
-		        ReadPropertyAck ack = (ReadPropertyAck) localDevice.send(d, new ReadPropertyRequest(oid,
-		                PropertyIdentifier.protocolServicesSupported));
-		        d.setServicesSupported((ServicesSupported) ack.getValue());
-		
-		        System.out.println("objectName");
-		        ack = (ReadPropertyAck) localDevice.send(d, new ReadPropertyRequest(oid, PropertyIdentifier.objectName));
-		        d.setName(ack.getValue().toString());
-		
-		        System.out.println("protocolVersion");
-		        ack = (ReadPropertyAck) localDevice.send(d, new ReadPropertyRequest(oid, PropertyIdentifier.protocolVersion));
-		        d.setProtocolVersion((UnsignedInteger) ack.getValue());
-		
-		        //        System.out.println("protocolRevision");
-		        //        ack = (ReadPropertyAck) localDevice.send(d, new ReadPropertyRequest(oid, PropertyIdentifier.protocolRevision));
-		        //        d.setProtocolRevision((UnsignedInteger) ack.getValue());
-    		}catch(Exception e){
-    			e.printStackTrace();
-    		}
-    	}
-    }
-    
-    
-    static class IncomingMessageExecutor extends Thread{
-    	
-    	RemoteDevice d;
-    	
-    	public IncomingMessageExecutor(RemoteDevice d){
-    		this.d = d;
-    	}
-    	
-    	
-    	@Override
-    	public void run(){
+    static class ExtendedDeviceInformationExecutor extends Thread {
 
-			try {
-	            List oids;
-				oids = ((SequenceOf) RequestUtils.sendReadPropertyAllowNull(localDevice, d,
-				        d.getObjectIdentifier(), PropertyIdentifier.objectList)).getValues();
-				 System.out.println(oids);
-			} catch (BACnetException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-           
-    	}
+        RemoteDevice d;
+
+        public ExtendedDeviceInformationExecutor(RemoteDevice d) {
+            this.d = d;
+        }
+
+        @Override
+        public void run() {
+            try {
+                ObjectIdentifier oid = d.getObjectIdentifier();
+                
+                // Get the device's supported services
+                System.out.println("protocolServicesSupported");
+                localDevice.send(d,
+                        new ReadPropertyRequest(oid, PropertyIdentifier.protocolServicesSupported));
+                //d.setServicesSupported((ServicesSupported) ack.getValue());
+
+                System.out.println("objectName");
+                localDevice.send(d,
+                        new ReadPropertyRequest(oid, PropertyIdentifier.objectName));
+                //d.setName(ack.getValue().toString());
+
+                System.out.println("protocolVersion");
+                localDevice.send(d,
+                        new ReadPropertyRequest(oid, PropertyIdentifier.protocolVersion));
+                //d.setProtocolVersion((UnsignedInteger) ack.getValue());
+
+                // System.out.println("protocolRevision");
+                // ack = (ReadPropertyAck) localDevice.send(d, new ReadPropertyRequest(oid,
+                // PropertyIdentifier.protocolRevision));
+                // d.setProtocolRevision((UnsignedInteger) ack.getValue());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
-    
-    
+
+
+    static class IncomingMessageExecutor extends Thread {
+
+        RemoteDevice d;
+
+        public IncomingMessageExecutor(RemoteDevice d) {
+            this.d = d;
+        }
+
+
+        @Override
+        public void run() {
+
+            try {
+                List<?> oids;
+                oids = ((SequenceOf<?>) RequestUtils.sendReadPropertyAllowNull(localDevice, d,
+                        d.getObjectIdentifier(), PropertyIdentifier.objectList)).getValues();
+                System.out.println(oids);
+            } catch (BACnetException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
 }
